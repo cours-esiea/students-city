fb99d09909694032c34c087a2ed7077cba6ecdc2
lsmod <1526305+lsmod@users.noreply.github.com>
2025-01-02 19:27:12 +0100

    working basic integration of openstreetmap

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,13 +2,15 @@ plugins {
     id("com.android.application")
 }
 
+
+
 android {
     namespace = "com.example.studentscity"
     compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.studentscity"
-        minSdk = 24
+        minSdk = 30
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -33,6 +35,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("org.osmdroid:osmdroid-android:6.1.17")
+    implementation("com.google.maps.android:android-maps-utils:3.8.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
---
+++ app/src/main/java/com/example/studentscity/MapActivity.java
@@ -0,0 +1,71 @@
+package com.example.studentscity;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.MapView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapActivity extends AppCompatActivity{
+    private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
+    private MapView map = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
+        //inflate and create the map
+        setContentView(R.layout.activity_map);
+
+        this.map = (MapView) findViewById(R.id.mapView);
+        this.map.setTileSource(TileSourceFactory.MAPNIK);
+
+        requestPermissionsIfNecessary(new String[]{
+                Manifest.permission.INTERNET,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        map.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        map.onPause();
+    }
+
+    private void requestPermissionsIfNecessary(final String[] permissions) {
+        final List<String> permissionsToRequest = new ArrayList<>();
+        for (final String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // Permission is not granted
+                permissionsToRequest.add(permission);
+            }
+        }
+        if (permissionsToRequest.size() > 0) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    permissionsToRequest.toArray(new String[0]),
+                    this.REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
+    }
+}
